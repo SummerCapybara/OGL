0de5a71e02227f3e6dc8837fbb9e3ebad3258958
SummerCapybara <prpl40712@gmail.com>
2025-12-17 02:33:20 +0300

    updated sigma.cpp

--- sigma.cpp
+++ sigma.cpp
@@ -2,22 +2,119 @@
 #include <GLFW/glfw3.h>
 #include <iostream>
 
-int main() {
-    glfwInit();
-    GLFWwindow* window = glfwCreateWindow(800, 600, "OGL", NULL, NULL);
-    glfwMakeContextCurrent(window);
+
+struct Engine {
+    GLFWwindow* window;
+    int WindowWidth = 800;
+    int WindowHeight = 600;
     
-    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
-        return -1;
+    static void FramebufferSizeCallback(GLFWwindow* window, int width, int height) {
+        Engine* engine = static_cast<Engine*>(glfwGetWindowUserPointer(window));
+        if (!engine) {
+            std::cerr << "no engine? FlamebufferSizecalback.";
+            glfwTerminate();
+            exit(EXIT_FAILURE);
+        }
+        engine -> WindowWidth = width;
+        engine -> WindowHeight = height;
+        // std::cout << width << "x" << height << "\n";
+        glViewport(0, 0, width, height);
+    };
+
+    static void KeyCallback(GLFWwindow* window, int key, int scancode, int action, int mods) {
+        Engine* engine = static_cast<Engine*>(glfwGetWindowUserPointer(window));
+
+        if (!engine) {
+            std::cerr << "no engine? KeyCallback.";
+            glfwTerminate();
+            exit(EXIT_FAILURE);
+        }
+
+        if (action == GLFW_PRESS) {
+            std::cout << key << "\n";
+        }
+    };
+
+    static void MouseCallback(GLFWwindow* window, int button, int action, int mods) {
+        Engine* engine = static_cast<Engine*>(glfwGetWindowUserPointer(window));
+        
+        if (!engine) {
+            std::cerr << "no engine? MouseCallback.";
+            glfwTerminate();
+            exit(EXIT_FAILURE);
+        }
+
+        std::cout << "Mouse button: " << button << " action: " << action << std::endl;
+    };
+
+    static void CursorPosCallback(GLFWwindow* window, double xpos, double ypos) {
+        Engine* engine = static_cast<Engine*>(glfwGetWindowUserPointer(window));
+        
+        if (!engine) {
+            std::cerr << "no engine? CursorPosCallback.";
+            glfwTerminate();
+            exit(EXIT_FAILURE);
+        }
+
+        if (xpos >= 400 && xpos <= 600 && ypos >= 300 && ypos <= 400) {
+            if (glfwGetMouseButton(window, GLFW_MOUSE_BUTTON_LEFT) == GLFW_PRESS) {
+                std::cout << "button pressed!\n";
+            }
+        }
     }
-    
-    // Now use OpenGL: glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
-    while (!glfwWindowShouldClose(window)) {
 
+    Engine() {
+
+        // HINTS
+        glfwWindowHint(GLFW_RESIZABLE, GLFW_TRUE);
+        glfwWindowHint(GLFW_VISIBLE, GLFW_TRUE);
+
+
+        if (!glfwInit()) {
+            std::cerr << "ur glfwInit is cooked twin\n";
+            glfwTerminate();
+            exit(EXIT_FAILURE);
+        };
+
+        window = glfwCreateWindow(WindowWidth, WindowHeight, "OGL", NULL, NULL);
+
+        if (!window) {
+            std::cerr << "no window?\n";
+            glfwTerminate();
+            exit(EXIT_FAILURE);
+        }
+
+        glfwMakeContextCurrent(window);
+
+        if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+            std::cerr << "no glad?\n";
+            glfwTerminate();
+            exit(EXIT_FAILURE);
+        }
+
+        glfwSetWindowUserPointer(window, this);
+        glfwSetFramebufferSizeCallback(window, FramebufferSizeCallback);
+        glfwSetKeyCallback(window, KeyCallback);
+        glfwSetMouseButtonCallback(window, MouseCallback);
+        glfwSetCursorPosCallback(window, CursorPosCallback);  
+    };
+
+        ~Engine() {
+            glfwDestroyWindow(window);
+            glfwTerminate();
+        };
+};
+
+
+int main() {
+    Engine engine;
+    while (!glfwWindowShouldClose(engine.window)) {
+        glClearColor(0.1f, 0.1f, 0.15f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
-        glClearColor(0.1f, 0.1f, 0.15f, 1.0f); // deep blue/gray
+        
 
-        glfwSwapBuffers(window);
+        glfwSwapBuffers(engine.window);
         glfwPollEvents();
     }
+
 }
